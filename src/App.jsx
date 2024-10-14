7c7a389d11d0a8d43459d919f53db428ff3b2063
Suvonov Javohir <suvonovjavohir625@gmail.com>
2024-10-14 12:19:50 +0500

    begin

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import gallerey from "./data";
+
+function App() {
+  const [images, setImages] = useState(gallerey);
+
+  const addLIke = () =>{
+    
+  }
+
+  return (
+    <div className="images-container">
+      {images.map((image) => {
+        return (
+          <div key={image.id}>
+            <img src={image.url} alt="" />
+            <div className="buttons-container">
+              <button>👍 {image.likes}</button>
+              <button>👎 {image.disLikes}</button>
+              <button>🗑</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
